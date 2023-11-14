d08a846f772510e3a34ee8f6dbfc0442459fac44
MiroTesting <149673876+MiroTesting@users.noreply.github.com>
2023-11-14 16:37:01 +0100

    Update Žiadosti SELECTs.sql

--- Žiadosti SELECTs.sql
+++ Žiadosti SELECTs.sql
@@ -1,10 +1,10 @@
 USE QUATRO
 
 ---------------------------------------------
---SELECTY k ûiadostiam, podpisu, CN, CIS...
+--SELECTY k ≈æiadostiam, podpisu, CN, CIS...
 ---------------------------------------------
 
---UUID, typ podpÌsanÈho dokumentu, forma podpisania,miesto podpisania, CN, barcode, CF, podpis NB, Ëas vytvorenia, party ID, prospect ID (ONLY SIGNED)
+--UUID, typ podp√≠san√©ho dokumentu, forma podpisania,miesto podpisania, CN, barcode, CF, podpis NB, ƒças vytvorenia, party ID, prospect ID (ONLY SIGNED)
 
 SELECT [eshop-dw-subscriber].[application_identifiers].business_key, [eshop-dw-subscriber].[application_identifiers].correlation_id, [eshop-dw-subscriber].[application_identifiers].channel, [eshop-dw-subscriber].[application_identifiers].contract_number, [eshop-dw-subscriber].[application_identifiers].barcode, [eshop-dw-subscriber].[application_identifiers].credit_flow_id, [eshop-dw-subscriber].[application_identifiers].application_signed_at, application_signed_form, [eshop-dw-subscriber].[application_identifiers].contract_signed_at, [eshop-dw-subscriber].[application_identifiers].contract_signed_form, [eshop-dw-subscriber].application_identifiers.created_at, [eshop-dw-subscriber].[application_identifiers].contract_nb_signed_at, [eshop-dw-subscriber].[application_identifiers].contract_nb_signed_form, [vub-client-manager].identification.party_id, [vub-client-manager].identification.prospect_id, [vub-client-manager].identification.result_data, [eshop-application-state].raw_state.detail, COUNT (*)  
 FROM [eshop-dw-subscriber].[application_identifiers]
@@ -15,7 +15,7 @@ HAVING COUNT(*) > 1
 ORDER by [eshop-dw-subscriber].[application_identifiers].created_at DESC
 
 
---Typ podpÌsanÈho dokumentu, forma podpisania, Ëas vytvorenia, party ID, prospect ID (ONLY SIGNED)
+--Typ podp√≠san√©ho dokumentu, forma podpisania, ƒças vytvorenia, party ID, prospect ID (ONLY SIGNED)
 USE QUATRO
 SELECT [quatro-document-starter].[document].[business_key], [quatro-document-starter].[document].document_type, [quatro-document-starter].[document].sign_form, [quatro-document-starter].[document].salesman_code, [quatro-document-starter].[document].created_at, [vub-client-manager].[identification].party_id, [vub-client-manager].[identification].prospect_id, [vub-client-manager].[identification].result_data, COUNT (*)
 FROM [quatro-document-starter].[document]
