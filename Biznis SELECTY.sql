666020f2fe22299de58dbdb7f2870605aee36aec
MiroTesting <149673876+MiroTesting@users.noreply.github.com>
2023-11-14 16:33:26 +0100

    Update and rename Biznis SELECT.sql to Biznis SELECTY.sql

--- Biznis SELECTY.sql
+++ Biznis SELECTY.sql
@@ -1,40 +1,40 @@
 USE QBUSINESS
 
 ----------------------------------------------
---SELECTY, ktorÈ mÙûe pouûiù biznis oddelenie
+--SELECTY, ktor√© m√¥≈æe pou≈æit biznis oddelenie
 ----------------------------------------------
 
---Vypiö ûiadosti, ktorÈ s˙ podpÌsanÈ v obdobÌ od 1 Septembra do 12 OktÛbra
-SELECT [contract_number] AS '»Ìslo ûiadosti',[applicant_first_name] AS 'Meno', [applicant_last_name] AS 'Priezvisko', [applicant_birth_number] AS 'RodnÈ Ëislo',[applicant_email] AS 'Email', [state_partner] AS 'Stav ûiadosti',[loan_product_name], [loan_product_description], [created_at] AS 'D·tum vytvorenia'
+--Vypi≈° ≈æiadosti, ktor√© s√∫ podp√≠san√© v obdob√≠ od 1 Septembra do 12 Okt√≥bra
+SELECT [contract_number] AS '√à√≠slo ≈æiadosti',[applicant_first_name] AS 'Meno', [applicant_last_name] AS 'Priezvisko', [applicant_birth_number] AS 'Rodn√© √®islo',[applicant_email] AS 'Email', [state_partner] AS 'Stav ≈æiadosti',[loan_product_name], [loan_product_description], [created_at] AS 'D√°tum vytvorenia'
 FROM [dbo].[application]
 WHERE state_partner = 'SIGNED' and created_at BETWEEN '2023-09-01 00:00:00.0000000 +02:00' AND '2023-10-12 00:00:00.0000000 +02:00'
 ORDER BY contract_number DESC
 
 
--- VypÌö celkov˝ poËet predajn˝ch sieti
-SELECT COUNT(id) AS 'Celkov˝ poËet predajnÌ'
+-- Vyp√≠≈° celkov√Ω pocet predajn√Ωch sieti
+SELECT COUNT(id) AS 'Celkov√Ω po√®et predajn√≠'
 FROM [dbo].[business_network]
 
--- VypÌö vöetky padnutÈ ûiadosti za mesiac September, kde miesto spadnutia bola app SPIN
+-- Vyp√≠≈° v≈°etky padnut√© ≈æiadosti za mesiac September, kde miesto spadnutia bola app SPIN
 SELECT *
 FROM [dbo].application_failed
 WHERE created_at BETWEEN '2023-09-01 00:00:00.0000000 +02:00' and '2023-10-01 00:00:00.0000000 +02:00' 
 					AND process_id = 'spin' 
 
- -- VypÌö vöetky mesta v ktor˝ch m·me predajnu sieù a vypÌö ich poËet
+ -- Vyp√≠≈° v≈°etky mesta v ktor√Ωch m√°me predajnu siet a vyp√≠≈° ich po√®et
  SELECT COUNT (DISTINCT address_city) 
  FROM [dbo].business_network
 
-  -- VypÌö celkov˙ sumu za ktor˙ klienti nakupili v poboËk·ch za September
- SELECT SUM(loan_amount) AS 'Celkov· suma'
+  -- Vyp√≠≈° celkov√∫ sumu za ktor√∫ klienti nakupili v pobo√®k√°ch za September
+ SELECT SUM(loan_amount) AS 'Celkov√° suma'
  FROM [dbo].[application]
  WHERE created_at BETWEEN '2023-09-01 00:00:00.0000000 +02:00' and '2023-10-01 00:00:00.0000000 +02:00' AND state_partner = 'SIGNED'
 
 
- -- VypÌö vöetky ûiadosti vytvorenÈ od do 
+ -- Vyp√≠≈° v≈°etky ≈æiadosti vytvoren√© od do 
  SELECT * from dbo.application WHERE created_at BETWEEN '2022-06-28 00:00:00.0000000 +02:00' AND '2022-10-28 00:00:00.0000000 +02:00'
 
-  -- VypÌö vöetky s.r.o (PO) siete, ktorÈ maju pri zodpovednej osobe uvedenÈ priezvisko
+  -- Vyp√≠≈° v≈°etky s.r.o (PO) siete, ktor√© maju pri zodpovednej osobe uveden√© priezvisko
  SELECT * 
  FROM [dbo].business_network
  WHERE company_type = 'PO' 
